677951fcce8dd5a939cb0193e323133581e8c0c4
Justin A <jarata@gmail.com>
2019-11-11 10:01:12 -0800

    Reset

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,52 +1,57 @@
+import sys
+sys.path.append('../queue_and_stack')
+from dll_queue import Queue
+from dll_stack import Stack
+
+
 class BinarySearchTree:
-	def __init__(self, value):
-		self.value = value
-		self.left = None
-		self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-	def insert(self, value):
-		if self.value:
-			if value < self.value:
-				if self.left is None:
-					self.left = BinarySearchTree(value)
-				else:
-					self.left.insert(value)
-			elif value > self.value:
-				if self.right is None:
-					self.right = BinarySearchTree(value)
-				else:
-					self.right.insert(value)
-		else:
-			self.value = value
+    # Insert the given value into the tree
+    def insert(self, value):
+        pass
 
-	def contains(self, target):
-		if self.value == target:
-			return True
-		elif self.value < target:
-			if self.right is None:
-				return False
-			else:
-				return self.right.contains(target)
-		elif self.value > target:
-			if self.left is None:
-				return False
-			else:
-				return self.left.contains(target)
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        pass
 
-	def get_max(self):
-	# 	find largest node
-	# keep going right to endpoint
-		max = self.value
-	# 	loop through list, update max value if going right, until end of tree
-	#  max value at end of tree
-		while max.right:
-			max = max.right
-		return max.value
+    # Return the maximum value found in the tree
+    def get_max(self):
+        pass
 
-	def for_each(self, cb):
-		res = []
-		if cb:
-			res.append(cb.value)
-			res = res + self.for_each(cb.left)
-			res = res + self.for_each(cb.right)
-		return res
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        pass
+
+    # DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        pass
+
+    # STRETCH Goals -------------------------
+    # Note: Research may be required
+
+    # Print In-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
